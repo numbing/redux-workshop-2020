9e5a5a52064be30b2588b157b466987b02cb6cdb
mir <mirnumbing@gmail.com>
2020-01-20 20:10:01 +0100

    really done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBarMain from './components/navbar/navbarMain'
 
 import Weather from "./components/weatherComponent/Weather";
 import StarWars from "./components/starwarsComponent/StarWars";
@@ -13,6 +14,7 @@ class App extends React.Component {
       <Provider store={store}>
         <Router>
           <div>
+            <NavBarMain/>
             <Switch>
               <Route exact path="/" component={StarWars} />
               <Route exact path="/weather" component={Weather} />
