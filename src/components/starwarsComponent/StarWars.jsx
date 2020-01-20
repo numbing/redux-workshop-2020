172d491baa989d83ec090f31e847c457a450e898
mir <mirnumbing@gmail.com>
2020-01-20 17:56:31 +0100

    display charechters

--- src/components/starwarsComponent/StarWars.jsx
+++ src/components/starwarsComponent/StarWars.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { getStarWarsData } from "./starwarsAction";
+import StarWarsCard from './StarWarsCard'
 
 class StarWars extends Component {
+  componentDidMount() {
+    this.props.getStarWarsData();
+  }
   render() {
     console.log(this.props);
     return (
-      <div>
-        <h1>i am star wars</h1>
+      <div className="starWarsMain">
+        <h1>
+          all charechter of startwars counted and they are :
+          {this.props.charechterCount}
+        </h1>
+        <StarWarsCard/>
       </div>
     );
   }
 }
 const mapStateToProps = state => ({
-    state:state 
+  charechterCount: state.starWarsReducer.starWarsData.count
 });
 
-export default connect(mapStateToProps)(StarWars);
+export default connect(mapStateToProps, { getStarWarsData })(StarWars);
---
+++ src/components/starwarsComponent/StarWarsCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const StarWarsCard = props => {
+  if (!props.results) {
+    return <div>Loading....</div>;
+  }
+  return (
+    <div className="starWarsCard">
+      {props.results.map((e, i) => {
+        return (
+          <ul key={i} className="dataWrapper">
+            <li>name: {e.name}</li>
+            <li>hair color: {e.hair_color}</li>
+            <li>eye color: {e.eye_color}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
+const mapStateToProps = state => ({
+  results: state.starWarsReducer.starWarsData.results
+});
+
+export default connect(mapStateToProps)(StarWarsCard);
